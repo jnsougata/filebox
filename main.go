ad1261321e870a4b7290879c768845ac3f804058
sougata <jnsougata@gmail.com>
2023-09-09 14:42:56 +0530

    minor changes

--- main.go
+++ main.go
@@ -8,25 +8,25 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	app := gin.Default()
+	app.GET("/", func(c *gin.Context) {
 		c.File("static/app.html")
 	})
-	r.GET("/manifest.json", func(c *gin.Context) {
+	app.GET("/manifest.json", func(c *gin.Context) {
 		c.File("manifest.json")
 	})
-	r.GET("/worker.js", func(c *gin.Context) {
+	app.GET("/worker.js", func(c *gin.Context) {
 		c.File("worker.js")
 	})
-	r.GET("/shared/:hash", SharedPage)
-	r.GET("/embed/:hash", EmbedFile)
-	r.Static("/assets", "assets")
-	r.Static("/scripts", "scripts")
-	r.Static("/styles", "styles")
-	r.POST("/__space/v0/actions", Action)
-	r.GET("/__space/actions", AppActions)
+	app.GET("/shared/:hash", SharedPage)
+	app.GET("/embed/:hash", EmbedFile)
+	app.Static("/assets", "assets")
+	app.Static("/scripts", "scripts")
+	app.Static("/styles", "styles")
+	app.POST("/__space/v0/actions", Action)
+	app.GET("/__space/actions", AppActions)
 
-	api := r.Group("/api")
+	api := app.Group("/api")
 	api.GET("/ping", Ping)
 	api.GET("/key", ProjectKey)
 	api.GET("/root", Root)
@@ -43,10 +43,10 @@ func main() {
 	api.POST("/push/:id", PushFileMeta)
 	api.POST("/accept", AcceptFileMeta)
 
-	actions := r.Group("/actions")
+	actions := app.Group("/actions")
 	actions.POST("/save", Save.Handler)
 
-	if err := r.Run(":8080"); err != http.ErrServerClosed {
+	if err := app.Run(":8080"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
 }
